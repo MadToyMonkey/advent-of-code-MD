73731baaee3b54dad60aff0af435a29e776e960e
Madison Derringer <madison.derringer@milwaukeetool.com>
2023-12-01 16:21:10 -0600

    grrr

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -3,3 +3,19 @@
 On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 Consider your entire calibration document. What is the sum of all of the calibration values?
 '''
+# get the numbers out of the line
+import csv
+import re
+
+## get data out of csv
+with open('Day 1\input.csv',newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+#print(data)
+for x in data:
+
+#print(num_list)
+
+# save the first and last digit together
+
+# sum the new two digit numbers
