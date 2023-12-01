207aa8a1adbffa017f9934325761a6f1a6298090
Madison Derringer <madison.derringer@milwaukeetool.com>
2023-12-01 10:08:11 -0600

    starting day 1 puzzle

---
+++ Day 1/day1.py
@@ -0,0 +1,5 @@
+# Advent of Code: Day 1
+'''
+On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+Consider your entire calibration document. What is the sum of all of the calibration values?
+'''
