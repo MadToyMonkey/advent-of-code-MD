07ebd22e093efeebdf02fdff41bb4a38f903667c
Madz Derringer <madderringer@gmail.com>
2023-12-04 22:28:49 -0600

    attempt start day 3

---
+++ Day 3/day3-1.py
@@ -0,0 +1,21 @@
+# Advent of Code Day 3 - 1
+'''
+The engineer explains that an engine part seems to be missing from the engine, but nobody can figure out which one. 
+If you can add up all the part numbers in the engine schematic, it should be easy to work out which part is missing.
+
+The engine schematic (your puzzle input) consists of a visual representation of the engine. 
+There are lots of numbers and symbols you don't really understand, but apparently any number adjacent to a symbol, even diagonally, is a "part number" and should be included in your sum. 
+(Periods (.) do not count as a symbol.)
+
+What is the sum of all of the part numbers in the engine schematic?
+'''
+
+import csv, re
+
+with open('Day 3\input.csv', newline='\n') as f:
+    reader = csv.reader(f,)
+    data = list(reader)
+
+for line in data:
+    num = re.search(r"\d+",str(line))
+    print(num)
